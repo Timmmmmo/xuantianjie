0630cafc58a15618584f1f627463d0e95852ad2a
Timmmmmo <hjzhao86@163.com>
2026-09-18 17:03:21 +0800

    feat: 更新 sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
 /* 玄天劫 · 离线缓存 Service Worker */
-const CACHE = "xuantianjie-v6.0.0";
+const CACHE = "xuantianjie-v2.0.0-sprint-a";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./game.js",
   "./manifest.webmanifest",
+  "./js/analytics.js",
+  "./js/meta.js",
+  "./js/daily.js",
+  "./js/modes.js",
   "./assets/bg-start.png",
   "./assets/char-sword.png",
   "./assets/char-mage.png",
